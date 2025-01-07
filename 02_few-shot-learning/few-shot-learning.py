56d08df18f97ccafcd95f9d44b837838f372a07b
Daniel Oraca <danieloraca@users.noreply.github.com>
2025-01-07 19:46:50 +0000

    02 - Few-shot learning

---
+++ 02_few-shot-learning/few-shot-learning.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+from openai import OpenAI
+
+client = OpenAI()
+
+# Prompt for summarization
+prompt = """
+    Summarize the following text:
+(movie): {movie}
+"""
+
+# Few-show examples
+examples = [
+    {"input": "Titanic",  "output": "Ship sinks"},
+    {"input": "The Matrix",  "output": "Virtual reality"},
+    {"input": "bnlasbls",  "output": "funny movie about nothing. takes place in a forest."}
+]
+
+movie = "what can you tell me about the bnlasbls movie?"
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant. "},
+        {"role": "user", "content": prompt.format(movie=examples[0]["input"])},
+        {"role": "assistant", "content": examples[0]["output"]},
+        {"role": "user", "content": prompt.format(movie=examples[1]["input"])},
+        {"role": "assistant", "content": examples[1]["output"]},
+        {"role": "user", "content": prompt.format(movie=examples[2]["input"])},
+        {"role": "assistant", "content": examples[2]["output"]},
+        {"role": "user", "content": prompt.format(movie=movie)}
+    ]
+)
+
+print(response.choices[0].message.content)
