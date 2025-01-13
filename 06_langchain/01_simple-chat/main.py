d0b81f3a83a28c0d9cfd09d8819574e9d6afa2fe
Daniel Oraca <danieloraca@users.noreply.github.com>
2025-01-13 16:45:45 +0000

    06 langchain simple

---
+++ 06_langchain/01_simple-chat/main.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the OpenAI API key
+api_key = os.getenv("OPENAI_API_KEY")
+
+# Initialize OpenAI client
+
+# Define the prompt
+system_prompt = "You are an assistant finding information about TV series."
+human_prompt = "What is the plot of the series The Big bang theory?"
+
+full_prompt = f"{system_prompt}\n\n{human_prompt}"
+
+# Generate the response
+response = client.chat.completions.create(model="gpt-3.5-turbo",
+messages=[
+    {"role": "system", "content": system_prompt},
+    {"role": "user", "content": human_prompt}
+],
+temperature=0)
+
+# Print the AI's response
+print(response.choices[0].message.content.strip())
